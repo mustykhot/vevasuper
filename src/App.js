b7a4420eb0ea245d6b5a311103cea84c9355e1c1
Raji Mustapha <rajimustapha30@gmail.com>
2022-08-08 17:45:31 +0100

    fix

--- src/App.js
+++ src/App.js
@@ -2,48 +2,51 @@ import "./App.css";
 import { useEffect, useState } from "react";
 
 function App() {
-  const [list, setList] = useState([]);
+  const [list, setList] = useState(null);
   const [busId, setBusId] = useState("");
   const [state, setSate] = useState("");
-  console.log(list);
   useEffect(() => {
-    fetch(
-      "https://veva-dev.herokuapp.com/api/v1/admin/bussiness/APPROVED?page=1"
-    )
+    fetch("https://api.getveva.co/api/v1/admin/bussiness/APPROVED?page=1")
       .then((res) => {
         console.log(res);
         return res.json();
       })
       .then((result) => {
-        console.log(result);
         setList(result);
       });
   }, []);
-  const onSubmit = () => {
+  const onSubmit = (e) => {
+    e.preventDefault();
     setSate("Loading");
-    fetch(
-      `https://veva-dev.herokuapp.com/api/v1/admin/bussiness/${busId}/approve`
-    )
+    fetch(`https://api.getveva.co/api/v1/admin/bussiness/${busId}/approve`)
       .then((res) => {
         console.log(res);
         return res.json();
       })
       .then((result) => {
-        console.log(result);
         setSate("Approved");
+        setTimeout(() => {
+          setSate("");
+        }, 3000);
       });
   };
   return (
     <div className="App">
       <form onSubmit={onSubmit}>
+        <p>Select Business</p>
         <select
           onChange={(e) => {
             setBusId(e.target.value);
           }}
         >
-          {/* {list.map((item, i) => {
-            return <option key={i}>{}</option>;
-          })} */}
+          {list &&
+            list.businesses.rows.map((item, i) => {
+              return (
+                <option value={item.id} key={i}>
+                  {item.name}
+                </option>
+              );
+            })}
         </select>
         <button>Approve</button>
         <p>{state}</p>
